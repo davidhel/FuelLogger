069df5740ea0601aeb8f29dec7859f5a416ba68d
davidhel <dav-h@hotmail.com>
2014-02-20 22:03:38 +0100

    La til mulighet for å registrere odometer

--- gen/no/appfortress/fuellogger/R.java
+++ gen/no/appfortress/fuellogger/R.java
@@ -26,19 +26,23 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
+        public static final int action_settings=0x7f080010;
         public static final int btnRegister=0x7f080001;
-        public static final int btnSubmit=0x7f08000a;
-        public static final int chcPrimary=0x7f080009;
+        public static final int btnSubmit=0x7f08000e;
+        public static final int chcPrimary=0x7f08000d;
+        public static final int editOdometer=0x7f08000b;
         public static final int editTankSize=0x7f080008;
         public static final int editVehicleBrand=0x7f080004;
         public static final int editVehicleModel=0x7f080006;
         public static final int imgCar=0x7f080002;
+        public static final int txtDistanceMeasure=0x7f08000c;
+        public static final int txtOdometer=0x7f08000a;
         public static final int txtRegistered=0x7f080000;
         public static final int txtTankSize=0x7f080007;
         public static final int txtVehicleBrand=0x7f080003;
         public static final int txtVehicleModel=0x7f080005;
-        public static final int txtVehicleName=0x7f08000b;
+        public static final int txtVehicleName=0x7f08000f;
+        public static final int txtVoulumeMeasure=0x7f080009;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -51,17 +55,21 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int brand=0x7f050006;
+        public static final int distanceMeasure=0x7f050011;
         public static final int exampleBrand=0x7f050008;
         public static final int exampleModel=0x7f050009;
+        public static final int exampleOdometer=0x7f050010;
         public static final int exampleTankSize=0x7f05000b;
         public static final int hello_world=0x7f050002;
         public static final int ifNotRegistered=0x7f05000c;
         public static final int model=0x7f050007;
         public static final int name=0x7f050004;
+        public static final int odometer=0x7f05000f;
         public static final int primary=0x7f050005;
         public static final int register=0x7f05000d;
         public static final int submit=0x7f050003;
         public static final int tankSize=0x7f05000a;
+        public static final int volumeMeasure=0x7f05000e;
     }
     public static final class style {
         /** 
