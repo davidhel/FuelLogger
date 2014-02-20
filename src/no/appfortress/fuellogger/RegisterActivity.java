ba2e28fc8f0d506dd37cee3bac828029395d8670
davidhel <dav-h@hotmail.com>
2014-02-20 15:35:29 +0100

    Jobbet videre med register acitivity, koblet aktiviteten sammen med main activity med knapper.

--- src/no/appfortress/fuellogger/RegisterActivity.java
+++ src/no/appfortress/fuellogger/RegisterActivity.java
@@ -1,7 +1,9 @@
 package no.appfortress.fuellogger;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends Activity{
 
@@ -12,6 +14,9 @@ public class RegisterActivity extends Activity{
 		setContentView(R.layout.activity_register);
 	}
 	
-	
+	public void btnSubmit(View view){
+		Intent intent = new Intent(this, MainActivity.class);
+		startActivity(intent);
+	}
 
 }
