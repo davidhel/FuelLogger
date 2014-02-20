aff225087edd5c75929003bec3d17171829beee5
davidhel <dav-h@hotmail.com>
2014-02-20 14:05:49 +0100

    Rettet opp fil som manglet og opprettet en klasse og en layout for registrering av kjøretøy

---
+++ src/no/appfortress/fuellogger/RegisterActivity.java
@@ -0,0 +1,17 @@
+package no.appfortress.fuellogger;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegisterActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_register);
+	}
+	
+	
+
+}
