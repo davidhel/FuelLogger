f8374546d2f4401d74b51ac8fb639be7b0dcb533
David Helgestad <dav-h@hotmail.com>
2014-03-11 10:39:49 +0100

    Prøvd å fikse maps, inneholder bugs

--- src/no/appfortress/fuellogger/MainActivity.java
+++ src/no/appfortress/fuellogger/MainActivity.java
@@ -1,21 +1,26 @@
 package no.appfortress.fuellogger;
 
-
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.View;
 
-public class MainActivity extends Activity {
-	
-	
+public class MainActivity extends FragmentActivity  {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//Use google api
+		
 	}
 
 	@Override
@@ -24,16 +29,20 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
 		// This checks if the device is compatible and up to date with Google Play Services
-		int status =  GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
-		System.out.println(status);
+			int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+			if(status == ConnectionResult.SUCCESS) {
+			    System.out.println();
+			}
+			else if(status == ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED){
+			}
 	}
 
-	public void btnRegister(View view){
+	public void btnRegister(View view) {
 		Intent intent = new Intent(this, RegisterActivity.class);
 		startActivity(intent);
 	}
