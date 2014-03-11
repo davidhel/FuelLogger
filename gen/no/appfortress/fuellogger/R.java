f95f93590f83976cf6bcced7b8d066fb70d8385e
David Helgestad <dav-h@hotmail.com>
2014-03-11 12:09:44 +0100

    Begynte å lage en registering for bensin påfyll

--- gen/no/appfortress/fuellogger/R.java
+++ gen/no/appfortress/fuellogger/R.java
@@ -26,34 +26,39 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080011;
-        public static final int btnRegister=0x7f080001;
-        public static final int btnSubmit=0x7f08000e;
-        public static final int chcPrimary=0x7f08000d;
-        public static final int editOdometer=0x7f08000b;
-        public static final int editTankSize=0x7f080008;
-        public static final int editVehicleBrand=0x7f080004;
-        public static final int editVehicleModel=0x7f080006;
-        public static final int imgCar=0x7f080002;
-        public static final int txtCarData=0x7f080010;
-        public static final int txtDistanceMeasure=0x7f08000c;
-        public static final int txtOdometer=0x7f08000a;
-        public static final int txtRegistered=0x7f080000;
-        public static final int txtTankSize=0x7f080007;
-        public static final int txtVehicleBrand=0x7f080003;
-        public static final int txtVehicleModel=0x7f080005;
-        public static final int txtVehicleName=0x7f08000f;
-        public static final int txtVoulumeMeasure=0x7f080009;
+        public static final int action_settings=0x7f080014;
+        public static final int btnRegister=0x7f080005;
+        public static final int btnSubmit=0x7f080012;
+        public static final int chcPrimary=0x7f080011;
+        public static final int editOdo=0x7f080003;
+        public static final int editOdometer=0x7f08000f;
+        public static final int editTankSize=0x7f08000c;
+        public static final int editVehicleBrand=0x7f080008;
+        public static final int editVehicleModel=0x7f08000a;
+        public static final int imgCar=0x7f080007;
+        public static final int imgFuel=0x7f080000;
+        public static final int txtCarData=0x7f080006;
+        public static final int txtDistanceMeasure=0x7f080010;
+        public static final int txtOdo=0x7f080002;
+        public static final int txtOdometer=0x7f08000e;
+        public static final int txtRegistered=0x7f080004;
+        public static final int txtTankSize=0x7f08000b;
+        public static final int txtVehicleBrand=0x7f080001;
+        public static final int txtVehicleModel=0x7f080009;
+        public static final int txtVehicleName=0x7f080013;
+        public static final int txtVoulumeMeasure=0x7f08000d;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_register=0x7f030001;
-        public static final int myvehicles_activity=0x7f030002;
+        public static final int activity_fueling=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_myvehicles=0x7f030002;
+        public static final int activity_register=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int Odometer=0x7f050012;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int brand=0x7f050006;
---
+++ src/no/appfortress/fuellogger/RegisterFuelingActivity.java
@@ -0,0 +1,16 @@
+package no.appfortress.fuellogger;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegisterFuelingActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_fueling);
+		
+	}
+	
+}
