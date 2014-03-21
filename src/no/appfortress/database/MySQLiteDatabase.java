f375ca63e60d1eefa04b4ba6b65c5753463fe4ac
morganbp <morgibp@gmail.com>
2014-03-21 18:58:54 +0100

    fiksa ordentlig

--- src/no/appfortress/database/MySQLiteDatabase.java
+++
@@ -1,8 +0,0 @@
-package no.appfortress.database;
-
-public abstract class MySQLiteDatabase {
-	
-	public static String databaseName = "FuelLoggerDatabase.db";
-	public static int databaseVersion = 5;
-	
-}
