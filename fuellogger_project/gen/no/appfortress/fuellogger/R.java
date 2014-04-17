7f7236124415d42d3507838ec18262dc4fad6a04
davidhel <dav-h@hotmail.com>
2014-04-17 16:13:20 +0200

    forbruks kalkulator

--- fuellogger_project/gen/no/appfortress/fuellogger/R.java
+++ fuellogger_project/gen/no/appfortress/fuellogger/R.java
@@ -1355,16 +1355,17 @@ containing a value of this type.
         public static final int abc_popup_menu_item_layout=0x7f030014;
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
-        public static final int activity_fueling=0x7f030017;
-        public static final int activity_gps=0x7f030018;
-        public static final int activity_main=0x7f030019;
-        public static final int activity_myvehicles=0x7f03001a;
-        public static final int activity_register=0x7f03001b;
-        public static final int database_layout=0x7f03001c;
-        public static final int fragment_whatisodo=0x7f03001d;
-        public static final int google_map_activity=0x7f03001e;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001f;
-        public static final int widget_layout=0x7f030020;
+        public static final int activity_calculator=0x7f030017;
+        public static final int activity_fueling=0x7f030018;
+        public static final int activity_gps=0x7f030019;
+        public static final int activity_main=0x7f03001a;
+        public static final int activity_myvehicles=0x7f03001b;
+        public static final int activity_register=0x7f03001c;
+        public static final int database_layout=0x7f03001d;
+        public static final int fragment_whatisodo=0x7f03001e;
+        public static final int google_map_activity=0x7f03001f;
+        public static final int support_simple_spinner_dropdown_item=0x7f030020;
+        public static final int widget_layout=0x7f030021;
     }
     public static final class menu {
         public static final int main=0x7f0e0000;
