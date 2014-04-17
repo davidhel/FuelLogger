5d0eaf522442fab63c32d2e504e38b4227bf5dc1
davidhel <dav-h@hotmail.com>
2014-04-17 21:40:37 +0200

    register car in database

--- fuellogger_project/src/no/appfortress/fuellogger/MyVehiclesActivity.java
+++ fuellogger_project/src/no/appfortress/fuellogger/MyVehiclesActivity.java
@@ -1,5 +1,8 @@
 package no.appfortress.fuellogger;
 
+import java.util.List;
+
+import no.appfortress.database.CarDBHandler;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,14 +20,23 @@ public class MyVehiclesActivity extends Activity{
 		
 		
 		
-		//Receive intent message with Bundle() from RegisterActivity.class
+		/*//Receive intent message with Bundle() from RegisterActivity.class
 		Intent intent = getIntent();
 		Bundle extras = intent.getExtras();
 		String carBrand = extras.getString(RegisterVehicleActivity.EXTRA_MESSAGE);
 		String carModel = extras.getString(RegisterVehicleActivity.EXTRA_MESSAGE2);
 		String tankSize = extras.getString(RegisterVehicleActivity.EXTRA_MESSAGE3);
-		String odometer = extras.getString(RegisterVehicleActivity.EXTRA_MESSAGE4);
+		String odometer = extras.getString(RegisterVehicleActivity.EXTRA_MESSAGE4);*/
+		//Get data from localDB
+		CarDBHandler database = new CarDBHandler(this);
+		List<Car> cars = database.getAllCars();
 		
-		txtCarData.setText("Merke: "+carBrand+" Modell: "+carModel);
+		String listString = "";
+		
+		/*for (String s : cars)
+		{
+		    listString += s + "\t";
+		}
+		txtCarData.setText("Merke: "+carBrand+" Modell: "+carModel);*/
 	}
 }
