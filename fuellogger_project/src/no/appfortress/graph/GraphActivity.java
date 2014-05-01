c091361f123e92f3c73f48ea16b4ed4d7a8ceb90
davidhel <dav-h@hotmail.com>
2014-05-01 20:57:39 +0200

    graph database

--- fuellogger_project/src/no/appfortress/graph/GraphActivity.java
+++ fuellogger_project/src/no/appfortress/graph/GraphActivity.java
@@ -1,5 +1,6 @@
 package no.appfortress.graph;
 
+import java.util.Calendar;
 import java.util.List;
 
 import no.appfortress.database.RefillDBHandler;
@@ -29,8 +30,11 @@ public class GraphActivity extends Activity {
 		for (int i = 0; i <= refills.size(); i++) {
 			Refill r = refills.get(i);
 			Calendar c = r.getDate();
-			float cons = 
-			Point p = MockData.getDataFromReceiver(Integer.parseInt(refills.get(i).getDate().), Integer.parseInt(refills.get(i).toString())); // We got new data!
+			int month = c.get(Calendar.MONTH);
+			int day = c.get(Calendar.DAY_OF_MONTH);
+			String date = day + "." + month;
+			double cons = r.getFuelPrice();
+			Point p = MockData.getDataFromReceiver(Double.parseDouble(date), cons); // We got new data!
 			line.addNewPoints(p); // Add it to our graph
 			// view.repaint();
 		}
--- fuellogger_project/src/no/appfortress/graph/MockData.java
+++ fuellogger_project/src/no/appfortress/graph/MockData.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class MockData {
 
 	// x is the day number, 0, 1, 2, 3
-	public static Point getDataFromReceiver(int x, int y)
+	public static Point getDataFromReceiver(double x, double y)
 	{
 		return new Point(x, y);
 	}
